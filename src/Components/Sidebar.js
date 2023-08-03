3073c7a6060cd4291849d2e3274957ea7e1e3ff2
Harry Steele <harrybsteele@gmail.com>
2023-08-03 12:03:51 -0400

    close button not working

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,7 +1,12 @@
 import "./Sidebar.css";
 import { useState } from "react";
 import hamburger from "./hamburger.png";
-import close from "./close.png"
+import close from "./close.png";
+import { NavLink } from "react-router-dom";
+import summary from "./summary.png";
+import transaction from "./transaction.png";
+import statistics from "./statistics.png";
+import product from "./product.png";
 
 const Sidebar = () => {
   const [hidden, setHidden] = useState(true);
@@ -11,7 +16,34 @@ const Sidebar = () => {
         <img id="hamburger-img" src={hamburger} />
       </button>
       <div id="sidebar" className={hidden ? "hidden" : "open"}>
-        <button id="close" onClick={() => setHidden(!hidden)}><img id="close-img" src={close} /></button>
+        <button id="close" onClick={() => setHidden(!hidden)}>
+          <img id="close-img" src={close} />
+        </button>
+
+        <NavLink to="/">
+          <div className="nav-link-container">
+            <img src={summary} className="sidebar-icon" />
+            <p>Summary</p>
+          </div>
+        </NavLink>
+        <NavLink to="/statistics">
+          <div className="nav-link-container">
+            <img src={statistics} className="sidebar-icon" />
+            <p>Statistics</p>
+          </div>
+        </NavLink>
+        <NavLink to="/transactions">
+          <div className="nav-link-container">
+            <img src={transaction} className="sidebar-icon" />
+            <p>Transactions</p>
+          </div>
+        </NavLink>
+        <NavLink to="/products">
+          <div className="nav-link-container">
+            <img src={product} className="sidebar-icon" />
+            <p>Products</p>
+          </div>
+        </NavLink>
       </div>
     </>
   );
