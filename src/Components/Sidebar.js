ec4e59ec1622d922a411c71d4e7448bd43ac5e96
Harry Steele <harrybsteele@gmail.com>
2023-08-03 12:32:29 -0400

    finished a working sidebar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -10,40 +10,43 @@ import product from "./product.png";
 
 const Sidebar = () => {
   const [hidden, setHidden] = useState(true);
+  console.log(hidden);
   return (
     <>
       <button id="hamburger" onClick={() => setHidden(!hidden)}>
         <img id="hamburger-img" src={hamburger} />
       </button>
-      <div id="sidebar" className={hidden ? "hidden" : "open"}>
+      <div id="sidebar" className={hidden ? "hidden" : "open sidebar"}>
         <button id="close" onClick={() => setHidden(!hidden)}>
           <img id="close-img" src={close} />
         </button>
 
-        <NavLink to="/">
-          <div className="nav-link-container">
-            <img src={summary} className="sidebar-icon" />
-            <p>Summary</p>
-          </div>
-        </NavLink>
-        <NavLink to="/statistics">
-          <div className="nav-link-container">
-            <img src={statistics} className="sidebar-icon" />
-            <p>Statistics</p>
-          </div>
-        </NavLink>
-        <NavLink to="/transactions">
-          <div className="nav-link-container">
-            <img src={transaction} className="sidebar-icon" />
-            <p>Transactions</p>
-          </div>
-        </NavLink>
-        <NavLink to="/products">
-          <div className="nav-link-container">
-            <img src={product} className="sidebar-icon" />
-            <p>Products</p>
-          </div>
-        </NavLink>
+        <div id="nav-links-container">
+          <NavLink to="/">
+            <div className="nav-link-container">
+              <img src={summary} className="sidebar-icon" />
+              <p>Summary</p>
+            </div>
+          </NavLink>
+          <NavLink to="/statistics">
+            <div className="nav-link-container">
+              <img src={statistics} className="sidebar-icon" />
+              <p>Statistics</p>
+            </div>
+          </NavLink>
+          <NavLink to="/transactions">
+            <div className="nav-link-container">
+              <img src={transaction} className="sidebar-icon" />
+              <p>Transactions</p>
+            </div>
+          </NavLink>
+          <NavLink to="/products">
+            <div className="nav-link-container">
+              <img src={product} className="sidebar-icon" />
+              <p>Products</p>
+            </div>
+          </NavLink>
+        </div>
       </div>
     </>
   );
