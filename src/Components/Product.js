873acc19208cf3118b45a6eb01a88eec49330d37
Harry Steele <harrybsteele@gmail.com>
2023-08-04 15:26:08 -0400

    updated styling

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,17 +1,21 @@
 import React from "react";
-import "./Product.css"
+import "./Product.css";
 
-const Product = ({name, image, orders, sales}) => {
-    return (
-        <>
-        <div className="product-container">
-            <img className="product-img" src={image} />
-            <h1>{name}</h1>
-            <p>Orders : {orders}</p>
-            <p>Total Sales : {sales}</p>
-        </div>
-        </>
-    )
-}
+const Product = ({ name, image, orders, sales }) => {
+  return (
+    <>
+      <div className="product-container">
+        <img className="product-img" src={image} />
+        <h1>{name}</h1>
+        <p>
+          <span>Orders</span> : {orders}
+        </p>
+        <p>
+          <span>Total Sales</span> : {sales}
+        </p>
+      </div>
+    </>
+  );
+};
 
-export default Product
+export default Product;
